48187f6d3a5488a649b852dc65847b4870085ad8
Corinna Fabre <corinna.fabre@gmail.com>
2020-05-14 13:19:13 -0700

    Added Pizza Controller tests for new pizza feature

--- test/controllers/pizzas_controller_test.rb
+++ test/controllers/pizzas_controller_test.rb
@@ -4,4 +4,73 @@ describe PizzasController do
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
+  before do 
+    @pizza = Pizza.create(
+      name: 'Butterbeer',
+      crust: 'Gluten free',
+      sauce: 'Butter',
+      cheese: 'Cream Cheese',
+      toppings: 'Butterscotch candies'
+    )
+  end
+
+  it 'must get index' do
+    get pizzas_path
+    must_respond_with :success
+  end
+
+  it 'must get show for valid ids' do
+    get pizza_path(@pizza.id)
+    must_respond_with :success
+  end
+
+  it 'will give an error for a nonexistant pizza' do
+    get pizza_path(-1)
+    must_respond_with :not_found
+  end
+
+  describe "new" do
+    # Your tests go here
+    it "can get the new_pizza_path" do
+      get new_pizza_path
+
+      must_respond_with :success
+    end
+
+    it "can get the new_passenger_trip_path" do
+      get new_pizza_path(Pizza.first.id)
+
+      must_respond_with :success
+    end
+  end
+
+  describe "create" do
+    # Your tests go here
+    it "can create a new pizza" do
+
+      # Arrange
+      pizza_hash = {
+        pizza: {
+          name: 'Oysters on a Half Shell',
+          crust: 'Gluten free',
+          sauce: 'Cream',
+          cheese: 'None',
+          toppings: 'Canned Oysters Chives Lemon Capers'
+        },
+      }
+      
+      # Act-Assert
+      expect {
+        post pizzas_path, params: pizza_hash
+      }.must_change "Pizza.count", 1
+      
+      new_pizza = Pizza.find_by(name: pizza_hash[:pizza][:name])
+      expect(new_pizza.sauce).must_equal pizza_hash[:pizza][:sauce]
+
+      
+      must_respond_with :redirect
+      must_redirect_to pizza_path(new_pizza.id)
+    end
+  end
+
 end
