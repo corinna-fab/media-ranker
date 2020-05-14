502f0bc48ad0e755f05e28a92b4c8aa77c64ca53
Corinna Fabre <corinna.fabre@gmail.com>
2020-05-14 09:19:45 -0700

    Edit pizza feature working

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -12,4 +12,35 @@ class PizzasController < ApplicationController
       return
     end 
   end
+
+  def edit
+    pizza_id = params[:id]
+    @pizza = Pizza.find_by(id: pizza_id)
+
+    if @pizza.nil?
+      head :not_found
+      return
+    end 
+  end
+
+  def update
+    @pizza = Pizza.find_by(id: params[:id])
+    if @pizza.nil? 
+      head :not_found
+      return
+    elsif @pizza.update(pizza_params)
+      redirect_to @pizza
+      return
+    else
+      render :edit, status: :bad_request
+    end
+  end
+
+  private
+
+  def pizza_params
+    return params.require(:pizza).permit(:name, :crust, :sauce, :cheese, :toppings)
+  end
+  
+
 end
