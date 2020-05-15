1e4ae6ce88a441d8b50030922f8571406de2fc5e
Corinna Fabre <corinna.fabre@gmail.com>
2020-05-15 16:55:31 -0700

    Working user login and user login message for both new and returning users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/login', to: 'users#login_form', as: "login"
-  post '/login', to: 'users#login'
-  post '/logout', to: 'users#logout', as: "logout"
-
-  get 'users/current', to: 'users#current', as: "current_user"
-
   root to: 'pizzas#index'
   resources :votes
     
@@ -16,5 +10,10 @@ Rails.application.routes.draw do
     resources :votes#, only: [:new, :show, :index]
   end
   
-  # resources :users
+  resources :users 
+  
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,17 +17,5 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  def login(name = 'Wednesday Addams')
-    user_hash = {
-      user: {
-        name: name
-      }
-    }
-
-    post login_path, params: user_hash
-
-    user = User.find_by(name: name)
-    return user
-  end
   # Add more helper methods to be used by all tests here...
 end
