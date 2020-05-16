c044777b3adf17bfac58802cc3d8bb86b3104855
Corinna Fabre <corinna.fabre@gmail.com>
2020-05-16 14:34:48 -0700

    Join table working in console

--- db/migrate/20200515162218_create_pizzas_temperatures_join.rb
+++ db/migrate/20200515162218_create_pizzas_temperatures_join.rb
@@ -1,6 +1,6 @@
-class CreatePizzasTemperaturesJoin < ActiveRecord::Migration[6.0]
+class CreatePizzasTemperaturesJoinTable < ActiveRecord::Migration[6.0]
   def change
-    create_table :pizzas_temperatures_joins do |t|
+    create_table :pizzas_temperatures do |t|
       t.belongs_to :pizza, index: true
       t.belongs_to :temperature, index: true
     end
