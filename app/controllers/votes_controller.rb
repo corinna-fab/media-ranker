d96b0d236c5832757a299b2559ff16b5e6d767d3
Corinna Fabre <corinna.fabre@gmail.com>
2020-05-19 08:30:36 -0700

    Minor updates before new branch construction

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,6 @@
 class VotesController < ApplicationController
+  #Creating a new vote effectively creates a record for both the pizza and the person (one id pointing to each in the record) -- on button click, that new record should be created
+
+  #Votes need to be filtered by both pizza_id and user_id depending on the page
+
 end
