d2568ee57a0333882f2438a45cc228dbe8e88b4f
Corinna Fabre <corinna.fabre@gmail.com>
2020-05-21 14:43:47 -0700

    Dropping tables and reseeding

--- db/migrate/20200515162218_create_pizzas_temperatures_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreatePizzasTemperaturesJoinTable < ActiveRecord::Migration[6.0]
-  def change
-    create_table :pizzas_temperatures do |t|
-      t.belongs_to :pizza, index: true
-      t.belongs_to :temperature, index: true
-    end
-  end
-end
--- db/migrate/20200521212802_fix_pizza_temperatures_join_table.rb
+++
@@ -1,7 +0,0 @@
-require_relative "20200515162218_create_pizzas_temperatures_join_table"
-
-class FixPizzaTemperaturesJoinTable < ActiveRecord::Migration[6.0]
-  def change
-    revert CreatePizzasTemperaturesJoinTable
-  end
-end
