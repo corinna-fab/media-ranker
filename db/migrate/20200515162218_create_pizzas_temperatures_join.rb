bb1ff1d596e59bbce369009c95470ce92fd003a0
Corinna Fabre <corinna.fabre@gmail.com>
2020-05-20 18:23:57 -0700

    Migration error

--- db/migrate/20200515162218_create_pizzas_temperatures_join.rb
+++ db/migrate/20200515162218_create_pizzas_temperatures_join.rb
@@ -1,4 +1,4 @@
-class CreatePizzasTemperaturesJoinTable < ActiveRecord::Migration[6.0]
+class CreatePizzasTemperaturesJoin < ActiveRecord::Migration[6.0]
   def change
     create_table :pizzas_temperatures do |t|
       t.belongs_to :pizza, index: true
